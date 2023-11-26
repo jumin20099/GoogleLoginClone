0b6ad1912ebfd256a66a6574167f2cf2dd562726
jumin20099 <jumin20099@gmail.com>
2023-11-27 06:40:02 +0900

    디자인-수정

--- static/script.js
+++ static/script.js
@@ -1,13 +1,13 @@
 
 checkbox.addEventListener("click", function() {
-    
-    let pwInput = document.querySelector("#password");
-    let checkBox = document.querySelector("#checkbox")
 
-    if (checkBox.checked) {
+    let pwInput = document.querySelector("#password");
+    let checkBox = document.querySelector("#checkbox");
+
+    if (checkBox.checked){
         pwInput.type = 'text';
     }
-    else {
+    else{
         pwInput.type = 'password';
     }
 });
